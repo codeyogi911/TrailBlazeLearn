694f2a2eafaee84097faf4098f4d92de6e464e42
Romila <ms.romila@gmail.com>
2018-03-01 21:03:14 +0530

    Trail Exception class

---
+++ app/src/main/java/edu/nus/trailblazelearn/model/LearningTrail.java
@@ -0,0 +1,16 @@
+package edu.nus.trailblazelearn.model;
+
+/**
+ * Created by Hp on 01-03-2018.
+ */
+
+public class LearningTrail {
+
+    private static final String TAG = "LEARNINGTRAIL";
+
+    private int trailId;
+    private String trailName;
+    private String trailCode;
+    private String trailDescription;
+
+}
