9e3462646e9426b2ba818c49890cb9db5f4c19fb
ragupathy1294@gmail.com <ragupathy1294@gmail.com>
2018-03-12 19:52:00 +0800

    DbUtil class commit

--- app/src/main/java/edu/nus/trailblazelearn/utility/dbUtil.java
+++ app/src/main/java/edu/nus/trailblazelearn/utility/dbUtil.java
@@ -1,24 +1,52 @@
 package edu.nus.trailblazelearn.utility;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import edu.nus.trailblazelearn.adapter.ParticipantItemAdapter;
+import edu.nus.trailblazelearn.model.LearningTrail;
+import edu.nus.trailblazelearn.model.ParticipantItem;
+import edu.nus.trailblazelearn.model.UploadedFiles;
 
 public final class dbUtil {
     private static final String TAG = "dbUtil";
     public static FirebaseFirestore db;
+    private static StorageReference storageReference;
+    public static Uri fileUri;
+    public static ArrayList<ParticipantItem> participantItemArrayList = new ArrayList<>();
+    public static ArrayList<String> imageUriList = new ArrayList<>();
+    public static ArrayList<String> videoUriList = new ArrayList<>();
+    public static ArrayList<String> audioUriList = new ArrayList<>();
+    public static ArrayList<String> documentUriList = new ArrayList<>();
+    private static ArrayList<String> fileTypes;
 
     static{
         db = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
     }
 
     QuerySnapshot lastRead;
@@ -47,6 +75,84 @@ public final class dbUtil {
                 });
     }
 
+    public static void addObjectToDB(String collectionName, Object data) {
+        db.collection(collectionName)
+                .add(data)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+    //To do.. saving image/video/audio/document files to db
+    public static String addFilesToDB(final String userEmail, final String child, final Uri uri, final Context context, final String resultMessage, final ProgressBar progressBar, final String check) {
+        storageReference = storageReference.child(userEmail).child(child);
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(context, resultMessage, Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
+                if(check.equals("image")) {
+                    imageUriList.add(taskSnapshot.getDownloadUrl().toString());
+                }
+                if(check.equals("video")) {
+                    videoUriList.add(taskSnapshot.getDownloadUrl().toString());
+                }
+                if(check.equals("audio")) {
+                    audioUriList.add(taskSnapshot.getDownloadUrl().toString());
+                }
+                if(check.equals("document")) {
+                    documentUriList.add(taskSnapshot.getDownloadUrl().toString());
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(context, "Error Uploading", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+        return resultMessage;
+    }
+
+    public static void getParticipantItems(String userEmail, ProgressBar progressBar, ParticipantItemAdapter participantItemAdapter) {
+        final ProgressBar progressBarFinal = progressBar;
+        final ParticipantItemAdapter participantItemAdapterFinal = participantItemAdapter;
+        db.collection("participantActivities").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot documentSnapshots) {
+                participantItemArrayList = (ArrayList<ParticipantItem>) documentSnapshots.toObjects(ParticipantItem.class);
+                participantItemAdapterFinal.notifyDataSetChanged();
+                progressBarFinal.setVisibility(View.INVISIBLE);
+            }
+
+        });
+
+    }
+
+    public static Uri getFileUriFronDb(String userEmail, String child) {
+        storageReference = storageReference.child(userEmail).child(child);
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                fileUri = uri;
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("", "Error getting file URI");
+            }
+        });
+        return fileUri;
+    }
+
     /**
      * API to persist an object
      * into DB
@@ -71,6 +177,40 @@ public final class dbUtil {
     }
 
 
+/**
+     * API to fetch list of learning trails
+     * for logged in Trainer
+     * @param trainerId
+     * @return
+     */
+    public static List<LearningTrail> fetchTrailList(String trainerId){
+         final List<LearningTrail> learningTrailLst = new ArrayList<LearningTrail>();
+
+        // [START listen_multiple]
+        db.collection("LearningTrail")
+                .whereEqualTo("userId","ms.romila@gmail.com" )
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        Log.w(TAG,"Query snapshot :"+value.getQuery());
+                        for (DocumentSnapshot doc : value.getDocuments()) {
+                            LearningTrail learningTrailObj = doc.toObject(LearningTrail.class);
+                            learningTrailLst.add(learningTrailObj);
+                        }
+
+                        Log.d(TAG, "Current learning trail list size for trainer: "+learningTrailLst.size());
+                    }
+                });
+        // [END listen_multiple]
+
+        return learningTrailLst;
+    }
     //      Query DB using key and value
     public Task<QuerySnapshot> readWithKey(String collectionName, String key, String value) {
         return db.collection(collectionName).whereEqualTo(key, value)
