690acba27f58c9d659810068035ea2bc6a4a88b3
Romila <ms.romila@gmail.com>
2018-03-12 17:51:46 +0530

    Model class for LearningTrail

--- app/src/main/java/edu/nus/trailblazelearn/model/LearningTrail.java
+++ app/src/main/java/edu/nus/trailblazelearn/model/LearningTrail.java
@@ -1,16 +1,85 @@
 package edu.nus.trailblazelearn.model;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.nus.trailblazelearn.utility.dbUtil;
+
 /**
- * Created by Hp on 01-03-2018.
+ * Created by RMukherjee.
  */
 
 public class LearningTrail {
 
-    private static final String TAG = "LEARNINGTRAIL";
+    private static final String TAG = "LearningTrail.CLASS";
 
-    private int trailId;
     private String trailName;
     private String trailCode;
     private String trailDescription;
+    private Date startDate;
+    private Date endDate;
+    private String userId;
+
+    public LearningTrail() {
+        this.trailName = trailName;
+        this.trailCode = trailCode;
+        this.trailDescription = trailDescription;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.userId = userId;
+    }
+
+    public String getTrailName() {
+        return trailName;
+    }
+
+    public void setTrailName(String trailName) {
+        this.trailName = trailName;
+    }
+
+    public String getTrailCode() {
+        return trailCode;
+    }
+
+    public void setTrailCode(String trailCode) {
+        this.trailCode = trailCode;
+    }
+
+    public String getTrailDescription() {
+        return trailDescription;
+    }
+
+    public void setTrailDescription(String trailDescription) {
+        this.trailDescription = trailDescription;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
 }
