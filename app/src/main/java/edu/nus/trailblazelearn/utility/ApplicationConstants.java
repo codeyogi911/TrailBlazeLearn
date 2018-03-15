bd21297b16bb0af5e322ac596ce1b7ef6b12ace2
Romila <ms.romila@gmail.com>
2018-03-15 10:58:45 +0530

    Constant file created .It holds Application,DB,Helper,Error message as constants.

---
+++ app/src/main/java/edu/nus/trailblazelearn/utility/ApplicationConstants.java
@@ -0,0 +1,25 @@
+package edu.nus.trailblazelearn.utility;
+
+/**
+ * Created by RMukherjee on 15-03-2018.
+ */
+
+public interface ApplicationConstants {
+
+    /**Activity and Adapter related constants**/
+    String trailActivityListClassName = "LearningTrailListActivity";
+    String trailActivityCreateClassName ="CreateLearningTrailActivity";
+    String trailCodeParam = "trailCode";
+
+    /***DB Collection related constants**/
+    String learningTrailCollection ="LearningTrail";
+    String userId="userId";
+
+    /**Toast messages**/
+    String toastMessageForDbFailure = "An error occurred,please try later!";
+    String toastMessageForAddingTrailFailure="New trail creation failed,please try later!";
+    String getToastMessageForUpdateTrailFailure="Trail updation failed,please try later!";
+
+    /**Error messages*/
+    String errorDbSelectionForCollectionFailed = "Listen failed while selecting from collection Learning Trail.";
+}
